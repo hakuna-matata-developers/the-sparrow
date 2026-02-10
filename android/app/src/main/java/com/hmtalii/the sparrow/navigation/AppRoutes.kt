8723a43f03f0ed3dccd43407bd541192ccee0219
hakuna-matata-developers <hirammukaria@gmail.com>
2026-02-10 06:24:11 +0000

    Add app navigation routes for core H.MTALII features

---
+++ android/app/src/main/java/com/hmtalii/the sparrow/navigation/AppRoutes.kt
@@ -0,0 +1,11 @@
+package com.hmtalii.thesparrow.navigation
+
+object AppRoutes {
+    const val HOME = "home"
+    const val ANIMALS = "animals"
+    const val PARKS = "parks"
+    const val QUIZ = "quiz"
+    const val RAFIKI = "rafiki"
+    const val DAILY = "daily"
+    const val PROFILE = "profile"
+}
